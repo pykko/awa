13a24bc327b43117ee113cedfa2bca15b4c39b1a
pykko <pykko@xilim.fr>
2018-12-12 17:51:09 +0100

    Client - Base - Connexion

--- XDQi/app/clientXDQ.py
+++ XDQi/app/clientXDQ.py
@@ -10,7 +10,5 @@ def main() :
     vuePrincipale = VuePrincipale()
     vuePrincipale.mainloop()
 
-
-
 if __name__ == '__main__' :
     main()
--- XDQi/app/vueConnexion.py
+++ XDQi/app/vueConnexion.py
@@ -2,12 +2,21 @@
 # coding: utf-8
 
 from tkinter import *
+from requests import *
+
+
 
 class VueConnexion ( Toplevel ) :
 
     def __init__( self , parent ) :
         super().__init__( parent )
         self.title( 'Connexion' )
+
+        Label( self , text='Pseudo :' ).pack()
+        Label( self , text='MDP :' ).pack()
+
+
+        Button( self , text='Se connecter' , command = self.seConnecter ).pack()
 
 
     def seConnecter( self ) :
